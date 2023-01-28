1e9a45e8f0f0e1dde818e36203ecc7dad626dd98
SaVance Ford <savancef@gmail.com>
2023-01-28 16:21:28 -0600

    Add default state for break length, add handleClick function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Length from "./components/Length";
 
+const handleClick = (event) => {
+  // handle rather button is decrement or increment
+  console.log(event.target.className);
+};
+
 function App() {
+  const [breakState, setBreakState] = useState(5);
+
   return (
     <>
       <div className="clock-container">
         <h1>25 + 5 Clock</h1>
         <div className="length-container">
-          <Length type="Break" />
-          <Length type="Session" />
+          <Length type="Break" handleClick={handleClick} value={breakState} />
+          <Length type="Session" handleClick={handleClick} />
         </div>
         <div className="timer">
           <p>00:00</p>
--- src/components/Length.js
+++ src/components/Length.js
@@ -1,13 +1,17 @@
 export default function Length(props) {
-  const { type } = props;
+  const { type, handleClick, value } = props;
   return (
     <>
       <div className="dec-inc-container">
         <p>{type} Length</p>
         <div className="btns-container">
-          <button>Decrement</button>
-          <p>Value</p>
-          <button>Inecrement</button>
+          <button className={type + "-dec"} onClick={handleClick}>
+            Decrement
+          </button>
+          <p>{value}</p>
+          <button className={type + "-inc"} onClick={handleClick}>
+            Inecrement
+          </button>
         </div>
       </div>
     </>
