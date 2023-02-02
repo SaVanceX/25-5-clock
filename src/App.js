dada0667789246ed1d2235c0493f91beb41e05ca
SaVance Ford <savancef@gmail.com>
2023-02-02 15:31:35 -0600

    Handle seconds logic for session feature for counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState, useRef } from "react";
 import Length from "./components/Length";
 
 function App() {
   const [breakState, setBreakState] = useState(5);
-  const [sessionState, setSessionState] = useState(25);
+  const [sessionMin, setSessionMin] = useState(25);
+  const [sessionSeconds, setSessionSeconds] = useState(0);
+  const [togglePlay, setTogglePlay] = useState(false);
+  let intervalId = useRef(null);
 
   const handleClick = (event) => {
     const target = event.target.className;
@@ -13,10 +16,45 @@ function App() {
       console.log("Increment Break Value");
       setBreakState(breakState + 1);
     }
-    if (sessionState > 1 && "Session-dec" === target) {
-      setSessionState(sessionState - 1);
-    } else if (sessionState < 60 && "Session-inc" === target) {
-      setSessionState(sessionState + 1);
+    if (sessionMin > 1 && "Session-dec" === target) {
+      setSessionMin(sessionMin - 1);
+    } else if (sessionMin < 60 && "Session-inc" === target) {
+      setSessionMin(sessionMin + 1);
+    }
+  };
+
+  const startCountDown = () => {
+    console.log("Start Count Down");
+    intervalId.current = setInterval(() => {
+      setSessionSeconds((sessionSeconds) => {
+        if (sessionSeconds === 0) {
+          setSessionSeconds(59);
+        }
+        return sessionSeconds - 1;
+      });
+    }, 1000);
+  };
+  const stopCountDown = () => {
+    console.log("Pause count down");
+    clearInterval(intervalId.current);
+    intervalId.current = null;
+  };
+  // Function that handles Minutes and Seconds
+  const handleTogglePlayPause = () => {
+    setTogglePlay(!togglePlay);
+
+    if (togglePlay === false) {
+      if (sessionSeconds === 0) {
+        setSessionSeconds(59);
+        startCountDown();
+      }
+      if (sessionSeconds < 59 && sessionSeconds > 0) {
+        startCountDown();
+      }
+    }
+
+    if (togglePlay) {
+      stopCountDown();
     }
   };
 
@@ -26,18 +64,20 @@ function App() {
         <h1>25 + 5 Clock</h1>
         <div className="length-container">
           <Length type="Break" handleClick={handleClick} value={breakState} />
-          <Length
-            type="Session"
-            handleClick={handleClick}
-            value={sessionState}
-          />
+          <Length type="Session" handleClick={handleClick} value={sessionMin} />
         </div>
-        {/* padStart(2, '0') */}
         <div className="session-timer">
-          <p>{String(sessionState).padStart(2, "0")}:00</p>
+          <p className="session-text">
+            {String(sessionMin).padStart(2, "0")}:
+            {String(sessionSeconds).padStart(2, "0")}
+          </p>
         </div>
         <div className="timer-controls">Timer controls</div>
-        <button>Play - Pause</button>
+        {!togglePlay ? (
+          <button onClick={handleTogglePlayPause}>Play</button>
+        ) : (
+          <button onClick={handleTogglePlayPause}>Pause</button>
+        )}
         <button>Refresh</button>
         <p>Designed and Developed By SaVance Ford</p>
       </div>
